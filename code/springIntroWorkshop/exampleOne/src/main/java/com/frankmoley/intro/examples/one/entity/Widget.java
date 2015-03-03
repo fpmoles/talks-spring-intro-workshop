e54be3c370918bfdd8643e3dd29ec3f86aa48ec9
Frank Moley <frank.moley@garmin.com>
2015-03-03 16:39:55 -0600

    Initial commit for example one

---
+++ code/springIntroWorkshop/exampleOne/src/main/java/com/frankmoley/intro/examples/one/entity/Widget.java
@@ -0,0 +1,8 @@
+package com.frankmoley.intro.examples.one.entity;
+
+/**
+ * @author Frank Moley
+ */
+public class Widget {
+    
+}
---
+++ code/springIntroWorkshop/exampleOne/src/main/java/com/frankmoley/intro/examples/one/service/WidgetService.java
@@ -0,0 +1,10 @@
+package com.frankmoley.intro.examples.one.service;
+
+/**
+ * @author Frank Moley
+ */
+public interface WidgetService {
+
+    Widget assembleWidget(String instructions);
+
+}
